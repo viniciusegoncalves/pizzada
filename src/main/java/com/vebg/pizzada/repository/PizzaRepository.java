45ce03ccdec7c73a380d2051e22d1c3a27e0391e
Vinícius Gonçalves <viniciusebg25@icloud.com>
2024-07-27 17:54:38 -0300

    Adjustments

--- src/main/java/com/vebg/pizzada/repository/PizzaRepository.java
+++ src/main/java/com/vebg/pizzada/repository/PizzaRepository.java
@@ -2,10 +2,8 @@ package com.vebg.pizzada.repository;
 
 import com.vebg.pizzada.entity.Pizza;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.repository.query.Param;
 
 import java.util.List;
-import java.util.Optional;
 
 public interface PizzaRepository extends JpaRepository<Pizza, Long> {
     List<Pizza> findAllByNomeContainingIgnoreCase(String nome);
