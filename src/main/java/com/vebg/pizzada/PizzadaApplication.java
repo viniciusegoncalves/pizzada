3adb1efce133369b40dbd11af3e43aaba2ad684d
Vinícius Gonçalves <viniciusebg25@icloud.com>
2024-07-27 15:40:31 -0300

    First commit

---
+++ src/main/java/com/vebg/pizzada/PizzadaApplication.java
@@ -0,0 +1,13 @@
+package com.vebg.pizzada;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PizzadaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PizzadaApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/vebg/pizzada/PizzadaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vebg.pizzada;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PizzadaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
